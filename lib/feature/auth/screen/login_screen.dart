c5707b025972167ec69dd8dd2e1c3f1be3c0a4a3
MosaadIbrahimB <mosaadibrahim85@gmail.com>
2025-04-22 00:19:14 +0200

    add login screen +finish all but  todo icon in footer screen

--- lib/feature/auth/screen/login_screen.dart
+++ lib/feature/auth/screen/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:modern_authentication_app/core/widget/logo_widget.dart';
+import 'package:modern_authentication_app/utils/app_color.dart';
 
 import '../widget/password_text_form_field_widget.dart';
 import '../widget/text_form_field_widget.dart';
@@ -65,8 +66,29 @@ class LoginScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       Text(
-                        "Forgot password?",
-                        style: AppTextStyle.roboto14BlackW400
+                        "Forgot Password ?",
+                        style: AppTextStyle.roboto14BlackW700
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+
+                  ButtonWidget(),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: AppTextStyle.roboto14BlackW700
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        "Sign up",
+                        style: AppTextStyle.roboto14BlackW700.copyWith(
+                          color: AppColor.primary,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                     ],
                   ),
@@ -80,11 +102,61 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: Offset(0, 5), // اتجاه الظل
+            ),
+          ],
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.blue,
+          gradient: LinearGradient(colors: [
+            AppColor.primary,
+            AppColor.secondary
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+      
+          ),
+      
+        ),
+        width: 199,
+        height: 53,
+        child: Text("LOG IN",
+          style: AppTextStyle.roboto16WhiteW700,
+      
+        ),
+      
+      ),
+    );
+  }
+}
+
 class AppTextStyle {
-  static TextStyle roboto14BlackW400 = TextStyle(
+  static TextStyle roboto14BlackW700 = TextStyle(
     fontSize: 14,
-    fontWeight: FontWeight.w400,
-    color: Colors.black,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff515151),
+    fontFamily: "Roboto",
+  );
+  static TextStyle roboto16WhiteW700 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
     fontFamily: "Roboto",
   );
 }
