4bf949f68999ab777c12a3706f490ab9cf9286a3
MosaadIbrahimB <mosaadibrahim85@gmail.com>
2025-04-29 01:50:37 +0300

    finish logic cubit add sub product in bag and change count item

--- lib/feature/home/data/model/product_model.dart
+++ lib/feature/home/data/model/product_model.dart
@@ -1,15 +1,47 @@
-class ProductModel {
-  final String image;
-  final String title;
-  final String rating;
-  final String ratingCount;
-  final String price;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  ProductModel({
+part 'product_model.freezed.dart';
+
+part 'product_model.g.dart';
+
+@freezed
+@JsonSerializable()
+class ProductModel with _$ProductModel {
+  @override
+  final String id;
+  @override
+  final String image;
+  @override
+  final String title;
+  @override
+  final String rating;
+  @override
+  final String ratingCount;
+  @override
+  final String price;
+  @override
+   int count;
+
+     ProductModel({
+
+    required this.id,
     required this.image,
     required this.title,
     required this.rating,
     required this.ratingCount,
     required this.price,
+    this.count=0,
   });
+
+  // const factory ProductModel({
+  // required String image,
+  // required String title,
+  // required String rating,
+  // required String ratingCount,
+  // required String price,
+  // }) = _ProductModel;
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => _$ProductModelFromJson(json);
+  Map<String, Object?> toJson() => _$ProductModelToJson(this);
+
 }
